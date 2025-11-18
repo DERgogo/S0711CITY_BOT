e5308fcbe2d90f0804a860df0e2c64c36c7d40d2
DERgogo <gsilic@gmx.de>
2025-11-18 12:52:53 +0100

    Update web.py

--- app/web.py
+++ app/web.py
@@ -1,10 +1,17 @@
 from fastapi import FastAPI, Request
 from telegram import Update
 from telegram.ext import ApplicationBuilder
+
 from app.config import BOT_TOKEN
 
 telegram_app = ApplicationBuilder().token(BOT_TOKEN).build()
 fastapi_app = FastAPI()
+
+
+@fastapi_app.on_event("startup")
+async def startup_event():
+    # Telegram App muss initialisiert werden
+    await telegram_app.initialize()
 
 
 @fastapi_app.post("/")
