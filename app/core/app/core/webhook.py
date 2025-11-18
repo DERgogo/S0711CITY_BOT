83a0c643272f5935d0f26ab9b14e4946edf3c3c7
DERgogo <gsilic@gmx.de>
2025-11-18 11:12:17 +0100

    Delete app/core/app/core directory

--- app/core/app/core/webhook.py
+++
@@ -1,7 +0,0 @@
-from config import APP_URL, BOT_TOKEN, WEBHOOK_URL
-import httpx
-
-async def set_webhook():
-    url = f"https://api.telegram.org/bot{BOT_TOKEN}/setWebhook"
-    async with httpx.AsyncClient() as client:
-        await client.post(url, json={"url": WEBHOOK_URL})
