a328e494ea050f95870503329ff32d3c91469598
DERgogo <gsilic@gmx.de>
2025-11-18 12:19:02 +0100

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,6 +1,7 @@
 import os
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-APP_URL = os.getenv("APP_URL")  # https://deinproject.up.railway.app
+APP_URL = os.getenv("APP_URL")  # z.B. https://ravishing-learning.up.railway.app
+
 WEBHOOK_PATH = f"/webhook/{BOT_TOKEN}"
 WEBHOOK_URL = APP_URL + WEBHOOK_PATH
